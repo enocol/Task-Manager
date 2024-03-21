e0755d8af81228f84c94d99411e1001c6b035676
enocol <eno_col@yahoo.com>
2024-03-21 00:48:06 +0000

    Add a path for the new user resgitration form

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,13 +6,14 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('addtask/', views.addtask, name='addtask'),
-    # path('yourtasks/', views.yourtasks, name='yourtasks'),
-    # path('task/', views.yourtasks, name='task'),
+    path('yourtasks/', views.yourtasks, name='yourtasks'),
+    path('register/', views.register_user, name='register'),
     path('addtask/', views.addtask, name='addtask'),
     path('updatetask/<int:id>/', views.updatetask, name='updatetask'),
     path('taskdetails/<int:id>/', views.taskdetails, name='taskdetails'),
     path('taskdelete/<int:id>/', views.deletetask, name='deletetask'),
     path('completedtask/', views.completedtask, name='completedtask'),
     path('into/', views.into, name='into'),
-    path('yourtasks/', views.YourTask.as_view(), name='yourtasks'),
+    path('task/edit/<int:id>', views.edit_task, name='edittask'),
+    path('userprofile/', views.user_profile, name='userprofile'),
 ]
