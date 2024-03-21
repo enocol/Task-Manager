db043c8c6491788a621a035ea47effdc87f9f2cb
enocol <eno_col@yahoo.com>
2024-03-21 00:45:41 +0000

    Create a new user registrtion form to include first and last name

--- tasks/form.py
+++ tasks/form.py
@@ -1,12 +1,23 @@
 from . models import Task
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 
 class Addtask(forms.ModelForm):
     class Meta():
         model = Task
-        fields = ['title', 'description', 'due_date', 'completed']
+        fields = ['title', 'description', 'due_date', 'priority']
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+
+class EditTask(forms.ModelForm):
+    class Meta():
+        model = Task
+        fields = ['title', 'description', 'due_date', 'completed', 'priority']
         widgets = {
             'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
@@ -16,4 +27,12 @@ class Updatetask(forms.ModelForm):
     class Meta():
         model = Task
         fields = ['completed']
+
+class RegisterUser(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
        
