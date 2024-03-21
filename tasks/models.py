97fecbd1a1649bfa3f20e55df1d2e66bd4f90527
enocol <eno_col@yahoo.com>
2024-03-21 00:46:31 +0000

    Include an additional field in the Task model

--- tasks/models.py
+++ tasks/models.py
@@ -5,12 +5,19 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
+    OPTIONS = (
+        ('Low', 'Low'),
+        ('Normal', 'Normal'),
+        ('High', 'High'),
+        ('Urgent', 'Urgent'),
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField()
+    priority = models.CharField(max_length=7, choices=OPTIONS, default='Normal')
     completed = models.BooleanField(default=False)
 
     def __str__(self):
